b8798dc1a83d1e344482fe737ffc97e6ba0c3df9
Agustin Sly <agustin_sly@hotmail.com>
2021-07-11 15:53:21 -0300

    cambios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App :)"/>
+    <HelloWorld msg="Welcome to Your Vue.js App :) probando si hace deploy"/>
   </div>
 </template>
 
